48f1ddcd81129698d82d70fa1d4c978712755194
Bhanuprakash <bhanuprakashk4545@gmail.com>
2026-09-01 19:04:16 +0530

    More concepts

---
+++ Python/2.py
@@ -0,0 +1,19 @@
+print("Hello, World!")
+print("Hi, My name is Bhanuprakash")
+a="10"
+b=25
+print(int(a)+b)
+a=20
+b=5
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
+print(a//b) #floor division
+print(a**b) #exponentiation
+print(a<b)
+print(a>b)
+print(a+b-a*b) #boadmaas
+print(a+b-a+b*2**3) #boadmaas
+
--- Python/index.py
+++
@@ -1,5 +0,0 @@
-print("Hello, World!")
-print("Hi, My name is Bhanuprakash")
-a="10"
-b=25
-print(int(a)+b)
