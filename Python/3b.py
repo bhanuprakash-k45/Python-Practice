71adf4475b3d18ddc78b5065d45e0da1dc6aafcb
Bhanuprakash <bhanuprakashk4545@gmail.com>
2026-09-02 22:34:22 +0530

    indexing

---
+++ Python/3b.py
@@ -0,0 +1,29 @@
+first_name="bhanu"
+last_name="prakash"
+full_name=first_name+" "+last_name
+print(full_name) #concatenation
+
+message="Danger! "
+print(message*3)#repeatation
+
+print(message.upper())
+print(message.lower())
+print(message.strip()*3)
+print(message.replace("Danger","Warning"))
+
+name='Bhanu "Hi"'
+print(name)
+name='''Hi my name is
+Bhanuprakash'''
+print(name) #paragraph writing
+
+name="Bhanuprakash"
+print(name[10]) #indexing/accessing
+print(name[3:8])#slicing
+print(name[:9])
+print(name[5:10])
+print(name[-2])#accessing from last
+print(name[-5:-1])#slicing from last
+print(name[::5])#step slicing
+
+
