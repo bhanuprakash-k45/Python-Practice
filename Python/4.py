a94554f3676c2e788027af2ada243ac958599091
Bhanuprakash <bhanuprakashk4545@gmail.com>
2026-09-04 22:13:08 +0530

    5th day

---
+++ Python/4.py
@@ -0,0 +1,16 @@
+a=10
+a=a+100
+a+=100
+print(a)#assignment operator
+a=10
+b=20
+print(a==b)#comparision operator
+print(a<b)
+print(a!=b)
+print(a<=b)#comparision opertor
+x=5
+y=10
+z=15
+print(x>0 and y>5)
+print(x>10 or z>10)
+print(not(x>10))#logical operator
