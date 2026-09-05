d11273ef8db995cb19a2563b5e533ec265a83519
Bhanuprakash <bhanuprakashk4545@gmail.com>
2026-09-05 17:25:04 +0530

    6th day

---
+++ Python/5.py
@@ -0,0 +1,13 @@
+items=["Bru","Sugar","Milk"]#list
+print(items)
+print(items[0])#list in index
+print(items[-1])#list in index
+#modify list
+items.pop()
+items.append("Biscuit")#adding new item in list
+print(items)
+items.remove("Sugar")#remove item from list
+print(items)
+items.insert(1,"Egg")#insert item via index element
+print(items)
+ 
